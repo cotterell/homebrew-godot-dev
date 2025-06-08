fe5b6ef3f55df31aaaa888d310f0dd20f709aedb
Michael E. Cotterell <mepcotterell@gmail.com>
2025-06-08 08:29:51 -0400

    first commit

---
+++ Casks/g/godot-dev.rb
@@ -0,0 +1,29 @@
+cask "godot" do
+  version "4.5-dev5"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  url "https://github.com/godotengine/godot/releases/download/#{version}/Godot_v#{version}-stable_macos.universal.zip",
+      verified: "github.com/godotengine/godot/"
+  name "Godot Engine Development Release"
+  desc "Game development engine"
+  homepage "https://godotengine.org/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)[._-]dev(\d+)$/i)
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Godot.app", target: "Godot Dev.app"
+  binary "#{appdir}/Godot Dev.app/Contents/MacOS/Godot", target: "godot"
+
+  uninstall quit: "org.godotengine.godot"
+
+  zap trash: [
+    "~/Library/Application Support/Godot",
+    "~/Library/Caches/Godot",
+    "~/Library/Saved Application State/org.godotengine.godot.savedState",
+  ]
+end
